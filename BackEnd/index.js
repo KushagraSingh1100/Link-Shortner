268e42890a7e2dfd7a8275fd116a0da8561129bf
Kushagra <kushagrasingh1100@gmail.com>
2025-03-08 04:32:27 +0530

    backend changes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -9,15 +9,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/", urlRoute);
+
+app.use("/api", urlRoute);
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is working! 🚀" });
+});
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("connected to database");
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error("Database connection error:", err));
 
 export default app;
